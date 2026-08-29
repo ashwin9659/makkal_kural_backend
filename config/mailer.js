c5d20db9bcaf22bb4e5c5b35dae9796210852649
Ashwin <aswinsanjay5@gmail.com>
2026-08-29 17:13:27 +0530

    Refactor mailer configuration to improve DNS handling and timeout settings

--- config/mailer.js
+++ config/mailer.js
@@ -1,18 +1,23 @@
+const dns = require("dns");
 const nodemailer = require("nodemailer");
+
+// Prefer IPv4 instead of IPv6
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   requireTLS: true,
-  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 module.exports = transporter;
