5254c9853176ed95777a2311899d98ea7a750aad
Ashwin <aswinsanjay5@gmail.com>
2026-08-29 17:21:22 +0530

    Update mailer configuration to use STARTTLS and adjust port settings

--- config/mailer.js
+++ config/mailer.js
@@ -1,21 +1,29 @@
 const dns = require("dns");
 const nodemailer = require("nodemailer");
 
+// Force Node.js to prefer IPv4
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+
+  // Gmail STARTTLS
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
+  // Force IPv4 - important for Render
+  family: 4,
+
   connectionTimeout: 30000,
   greetingTimeout: 30000,
   socketTimeout: 30000,
+
+  requireTLS: true,
 });
 
 module.exports = transporter;
